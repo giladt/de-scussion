5a8adcea7197a7f63ca636ffc068c2ec27f5f88c
Gilad Tsabar <2517870+giladt@users.noreply.github.com>
2021-09-01 21:00:01 +0200

    store profile in metadata and listen to changes

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import './app.css';
 import { autoinject, bindable } from "aurelia-framework";
 import { EventAggregator } from 'aurelia-event-aggregator';
-import { ethers } from 'ethers';
 import axios from 'axios';
+
+import { Auth } from './resources/auth';
 
 import CeramicClient from '@ceramicnetwork/http-client';
 import { IDX, getLegacy3BoxProfileAsBasicProfile } from '@ceramicstudio/idx';
 
 interface IMessage {
+  _id: string,
   text: string,
   author: string,
-  profile: IProfile,
+  metadata: any,
   showReply: boolean,
 }
 
@@ -20,191 +22,74 @@ interface IProfile {
   name: string,
 }
 
-interface IAuth { 
-  data: {
-    success: boolean, 
-    message: string 
-  },
-  status: number,
-  statusText: string,
-  headers: {
-    'content-type': string,
-    'cache-control': string,
-    'content-length': string,
-  },
-  config: {
-    url: string,
-    method: string,
-    data: string,
-    headers: {
-      'Content-Type': string,
-      Accept: string,
-    },
-  },
-}
-
 @autoinject
 export class App {
-  public title = 'De-scussion';
-  public messages: Array<IMessage> = [];
+  private title = 'De-scussion';
+  private messages: Array<IMessage> = [];
   private message: string;
-  private auth: IAuth;
-  public comments: any;
-  public isConnected = false;
+  private comments: any;
   private idx: IDX;
   private chainId: string|number;
-  public wallet: {
-    address: string,
-  };
-  
+  private messagesService: NodeJS.Timeout;
   private inputRef: HTMLTextAreaElement;
-  private provider: ethers.providers.Web3Provider;
-  
-  // private serverWeb3 = new ethers.providers.InfuraProvider(null, process.env.INFURA_ID)
+  private auth: Auth = new Auth();
   
   constructor() {
     this.message = "";
-    this.wallet = {
-      address: '',
-    }
   }
 
-  toggleReply(message): void {
+  toggleReply(_id: string): void {
+    const message = this.messages.filter(message => message._id === _id)[0]
     message.showReply = !message.showReply;
   }
 
-  // async isConnected():Promise<boolean> {
-  //   this.provider = (window as any).ethereum;
-  //   if(await this.provider.selectedAddress) {
-  //     console.log('constructor provider:',this.provider);
-  //     this.wallet.address = await this.provider.selectedAddress;
-  //     return true;
-  //   }
-  //   return false;
-  // }
-
   public async attached():Promise<void> {
+
     const ceramic = new CeramicClient(process.env.CERAMIC_API_URL);
     this.idx = new IDX({ ceramic })
 
     this.inputRef.addEventListener('keydown', (e) => this.handleKeypress(e));
 
-    // // Force page refreshes on network changes
-    // {
-    //     // The "any" network will allow spontaneous network changes
-    //     const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-    //     provider.on("network", (newNetwork, oldNetwork) => {
-    //         // When a Provider makes its initial connection, it emits a "network"
-    //         // event with a null oldNetwork along with the newNetwork. So, if the
-    //         // oldNetwork exists, it represents a changing network
-    //         if (oldNetwork) {
-    //             window.location.reload();
-    //         }
-    //     });
+    await this.auth.init();
+    this.loadConversation()
+    const messagesElem = window.document.querySelector('.messages')
+    messagesElem.scrollTop = messagesElem.scrollHeight;
+
+    this.messagesService = global.setInterval(()=>this.loadConversation(), 1000);
+
+    // if (this.auth.isConnected) {
+    //   // metamask is connected
+    //   this.auth.wallet.address = (await this.auth.provider.listAccounts())[0];
+    //   console.log('metamask connected to', this.auth.wallet.address);
+    // } else {
+    //   // metamask is not connected
+    //   console.log('metamask not connected');
     // }
-
-    const { ethereum } = (window as any);
-    if (ethereum) {
-      this.provider = new ethers.providers.Web3Provider(ethereum);
-      this.chainId = await this.provider.getNetwork().then(network => network.chainId);
-
-      // Listen to network changes and reload the page
-      (window as any).ethereum.on("networkChanged", (networkId) => {
-        window.location.reload();
-        console.log('network changed:', networkId);
-      });
-
-      // Listen to account changes and update the wallet
-      (window as any).ethereum.on('accountsChanged', function (accounts) {
-        console.log('accounts changed:', accounts[0]);
-        if(accounts.length > 0) {
-          this.wallet = {
-            address: accounts[0]
-          };
-          this.isConnected = true;
-          window.location.reload();
-        } else {
-          this.isConnected = false;
-        }
-      })
-    }
-
-    this.loadConversation();
-
-    const accounts = await this.provider.listAccounts();
-    this.isConnected = accounts.length > 0;
-
-    if (this.isConnected) {
-      // metamask is connected
-      this.wallet.address = (await this.provider.listAccounts())[0];
-      console.log('metamask connected to', this.wallet.address);
-    } else {
-      // metamask is not connected
-      console.log('metamask not connected');
-    }
   }
 
-  async isValidAuth(): Promise<string> {
-    const signature = localStorage.getItem('signature');
-    if(!signature) return '';
-
-    console.log('signature:',this.wallet);
-    return await axios.post(
-      `https://theconvo.space/api/validateAuth?apikey=${ process.env.CONVO_API_KEY }`,
-      {
-        "signerAddress": this.wallet.address,
-        "token": JSON.parse(signature).message
-      },
-    ).then(res => {
-      console.log('valid auth success?', res.data);
-      if(res.data.success) {
-        return res.data.message;
-      }
-    }).catch(err => {
-      console.error(err.message)
-      return '';
-    });
+  deactivate(): void {
+    clearInterval(this.messagesService);
   }
 
   async addMessage(commentID = '', message = ''): Promise<void> {
     if(!message) return;
 
-    let token = "";
-    // if(await !this.isValidAuth()) {      
-      const auth = await this.signThread();
-      token = auth.message;
-    // } else {
-    //   token = JSON.parse(localStorage.getItem('signature')).message;
-    // }
+    const auth = await this.auth.signThread();
+    const token = auth.message;
 
     try {
+      // Profile imported from ceramic IDX
+      const profile = await this.loadProfile(this.auth.wallet.address);
       const res = await axios.post(`https://theconvo.space/api/comments?${commentID? 'replyTo=' + commentID + '&':''}apikey=${ process.env.CONVO_API_KEY }`, {
         'token': token,
-        'signerAddress': this.wallet.address,
+        'signerAddress': this.auth.wallet.address,
         'comment': message,
-        'threadId': `cl_descussion:${this.chainId}`,
+        'metadata': profile,
+        'threadId': `cl_descussion:${this.auth.chainId}`,
         'url': encodeURIComponent( process.env.APP_URL ),
       });
 
-      if(res.status === 200) {
-        const profile = await this.loadProfile(this.wallet.address);
-        this.messages.push({
-          author: this.wallet.address,
-          text: message,
-          profile: profile ? 
-          {
-            // Profile imported from ceramic IDX
-            ...profile
-          } as IProfile :
-          {
-            // No profile, use placeholder
-            address: this.wallet.address,
-            image: null,
-            name: '',
-          } as IProfile,
-        } as IMessage);
-        this.message = "";
-      }
+      if(res.status === 200) this.message = "";
     } catch (error) {
       console.error(error.message);
     }
@@ -212,14 +97,12 @@ export class App {
 
   async voteMessage(type: string): Promise<void> {
     let token = "";
-    if(!(await this.isValidAuth)) {
-      const auth = await this.signThread();
+    if(!(await this.auth.isValidAuth)) {
+      const auth = await this.auth.signThread();
       token = auth.message;
     } else {
       token = JSON.parse(localStorage.getItem('signature')).message;
     }
-
-
   }
 
   handleKeypress(event: KeyboardEvent): void {
@@ -227,67 +110,6 @@ export class App {
       event.preventDefault();
       this.addMessage('', this.message);
     }
-  }
-
-  // User pressed the "Connect to wallet" button
-  async connect():Promise<void> {
-    if(!(await this.provider.listAccounts()).length) {
-      const { ethereum } = (window as any);
-      this.provider = await new ethers.providers.Web3Provider(ethereum, 'any');
-      await this.provider.send("eth_requestAccounts", []);
-      this.wallet.address = (await this.provider.listAccounts())[0];
-
-      this.isConnected = true;
-    } else {
-      console.log('provider:',this.provider);
-      this.wallet.address = (await this.provider.listAccounts())[0];
-    }
-  }
-
-  async isMetaMaskConnected():Promise<void> {
-    const accounts = await this.provider.listAccounts();
-    this.isConnected = accounts.length > 0;
-  }
-
-  async signThread():Promise<{success: boolean, message: string}> {
-    const validation = await this.isValidAuth()
-    console.log('validation:',validation);
-
-    if(!validation) {
-      // Sample signature generation code using ethers.js
-      const timestamp = Date.now();
-      const signer = await this.provider.getSigner();
-      const signerAddress = await signer.getAddress();
-      const data = `I allow this site to access my data on The Convo Space using the account ${signerAddress}. Timestamp:${timestamp}`;
-
-      try {
-        const signature = await this.provider.send(
-          'personal_sign',
-          [ ethers.utils.hexlify(ethers.utils.toUtf8Bytes(data)), signerAddress.toLowerCase() ]
-        );
-
-        const auth = (await axios.post(
-          `https://theconvo.space/api/auth?apikey=${ process.env.CONVO_API_KEY }`, 
-          {
-            signerAddress,
-            signature,
-            timestamp,
-          }
-        ));
-
-        if(auth.data.success){
-          localStorage.setItem('signature', JSON.stringify(auth.data));
-        }
-      } catch (e) {
-        if(localStorage.getItem('signature')) {
-          localStorage.removeItem('signature');
-        }
-        console.log('User denied signature:', e);
-      }
-
-    }
-
-    return JSON.parse(localStorage.getItem('signature'));
   }
 
   async loadProfile(author: string): Promise<IProfile> {
@@ -312,20 +134,27 @@ export class App {
           image: profile.image.original.src.replace('ipfs://', 'https://ipfs.io/ipfs/'),
           name: profile.name,
         }
+      } else {
+        return {
+          address: author,
+          image: null,
+          name: null,
+        };
       }
     });
   }
 
   async loadConversation():Promise<void> {
-    this.messages = (await axios.get(`https://theconvo.space/api/comments?threadId=cl_descussion:${this.chainId}&apikey=${ process.env.CONVO_API_KEY }`)).data;
-    if(this.messages.length) {
-      for(const message of this.messages){
-        if(message.author) {
-          const profile = await this.loadProfile(message.author);
-          message.profile = profile;
-          message.showReply = false;
-        }
-      }
+    const newMessages = (await axios.get(`https://theconvo.space/api/comments?threadId=cl_descussion:${this.auth.chainId}&apikey=${ process.env.CONVO_API_KEY }`)).data;
+    
+    if(newMessages.length && newMessages.length !== this.messages.length) {
+      newMessages.map(async message => {
+        if(message && !message.metadata.address) message.metadata = await this.loadProfile(message.author);
+      });
+      this.messages = newMessages;
+      const messagesElem = await window.document.querySelector('.messages')
+      messagesElem.scrollTop = messagesElem.scrollHeight;
     }
+
   }
 }
---
+++ src/resources/auth.ts
@@ -0,0 +1,148 @@
+import { autoinject } from "aurelia-framework";
+import axios from 'axios';
+import { ethers } from 'ethers';
+
+interface IAuth { 
+  data: {
+    success: boolean, 
+    message: string 
+  },
+  status: number,
+  statusText: string,
+  headers: {
+    'content-type': string,
+    'cache-control': string,
+    'content-length': string,
+  },
+  config: {
+    url: string,
+    method: string,
+    data: string,
+    headers: {
+      'Content-Type': string,
+      Accept: string,
+    },
+  },
+}
+
+interface IWallet {
+  address: string,
+}
+
+@autoinject
+export class Auth {
+  public wallet: IWallet;
+  public provider: ethers.providers.Web3Provider;
+  public chainId: string | number;
+  public isConnected = false;
+
+  public async init(): Promise<void> {
+    const { ethereum } = (window as any);
+    this.provider = new ethers.providers.Web3Provider(ethereum);
+    this.chainId = await this.provider.getNetwork().then(network => network.chainId);
+    const accounts = await this.provider.listAccounts();
+    this.isConnected = accounts.length > 0;
+    if(accounts.length > 0) this.wallet = { address: accounts[0] };
+
+    if (ethereum) {
+      // Listen to network changes and reload the page
+      ethereum.on("networkChanged", (networkId) => {
+        window.location.reload();
+        console.log('network changed:', networkId);
+      });
+
+      // Listen to account changes and update the wallet
+      ethereum.on('accountsChanged', function (accounts) {
+        window.location.reload();
+      })
+    }
+
+  }
+
+  public async isValidAuth(): Promise<string> {
+    const signature = localStorage.getItem('signature');
+    if(!signature) return '';
+
+    return await axios.post(
+      `https://theconvo.space/api/validateAuth?apikey=${ process.env.CONVO_API_KEY }`,
+      {
+        "signerAddress": this.wallet.address,
+        "token": JSON.parse(signature).message
+      },
+    ).then(res => {
+      if(res.data.success) {
+        return res.data.message;
+      }
+    }).catch(err => {
+      console.error(err.message)
+      return '';
+    });
+  }
+
+  public async signThread():Promise<{success: boolean, message: string}> {
+    const validation = await this.isValidAuth()
+
+    if(!validation) {
+      // Sample signature generation code using ethers.js
+      const timestamp = Date.now();
+      const signer = await this.provider.getSigner();
+      const signerAddress = await signer.getAddress();
+      const data = `I allow this site to access my data on The Convo Space using the account ${signerAddress}. Timestamp:${timestamp}`;
+
+      try {
+        const signature = await this.provider.send(
+          'personal_sign',
+          [ ethers.utils.hexlify(ethers.utils.toUtf8Bytes(data)), signerAddress.toLowerCase() ]
+        );
+
+        const auth = (await axios.post(
+          `https://theconvo.space/api/auth?apikey=${ process.env.CONVO_API_KEY }`, 
+          {
+            signerAddress,
+            signature,
+            timestamp,
+          }
+        ));
+
+        if(auth.data.success){
+          localStorage.setItem('signature', JSON.stringify(auth.data));
+        }
+      } catch (e) {
+        if(localStorage.getItem('signature')) {
+          localStorage.removeItem('signature');
+        }
+        console.log('User denied signature:', e);
+      }
+
+    }
+
+    return JSON.parse(localStorage.getItem('signature'));
+  }
+
+  // User pressed the "Connect to wallet" button
+  public async connect():Promise<void> {
+    const { ethereum } = (window as any);
+    this.provider = await new ethers.providers.Web3Provider(ethereum, 'any');
+    const accounts = await this.provider.listAccounts();
+
+    if(!accounts.length) {
+      await this.provider.send("eth_requestAccounts", []);
+    }
+    if(accounts.length) {
+      this.wallet = {
+        address: accounts[0]
+      }
+    } else {
+      this.wallet = {
+        address: '',
+      }
+      this.isConnected = false;
+    }
+  }
+
+  async isMetaMaskConnected():Promise<void> {
+    const accounts = await this.provider.listAccounts();
+    this.isConnected = accounts.length > 0;
+  }
+
+}
