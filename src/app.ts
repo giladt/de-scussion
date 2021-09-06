68b96ea9a8a8dd535a4baffdb2f47f71e98311e5
Gilad Tsabar <2517870+giladt@users.noreply.github.com>
2021-09-06 07:30:15 +0200

    fixed scroll

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import { autoinject, bindable } from "aurelia-framework";
 import { EventAggregator } from 'aurelia-event-aggregator';
 import axios from 'axios';
 import { format, formatDistanceToNowStrict } from 'date-fns'
-
 import { Auth } from './resources/auth';
 
 import CeramicClient from '@ceramicnetwork/http-client';
@@ -14,6 +13,7 @@ interface IMessage {
   text: string,
   author: string,
   metadata: any,
+  replyTo?: string,
   upvote: number,
   downvote: number,
   timestamp: number,
@@ -57,9 +57,9 @@ export class App {
     this.inputRef.addEventListener('keydown', (e) => this.handleKeypress(e));
 
     await this.auth.init();
-    this.loadConversation();
-    const messagesElem = window.document.querySelector('.messages')
-    messagesElem.scrollTop = messagesElem.scrollHeight;
+    await this.loadConversation();
+    const messagesElem = window.document.querySelector('.container');
+    window.scrollTo(0,messagesElem.scrollHeight);
 
     this.messagesService = global.setInterval(()=>this.loadConversation(), 1000);
   }
@@ -121,18 +121,28 @@ export class App {
   }
 
   async loadProfile(author: string): Promise<IProfile> {
-    // // Get the IDX profile
-    // return await idx.get('basicProfile', message.author + '@eip155:1').then((profile) => {
-    //   // Currently IDX.get returns Promise<unknown>
-    //   // Follow changes on: https://developers.idx.xyz/reference/idx/#get
-    //   if(profile !== null) {
-    //     return {
-    //       address: message.author,
-    //       image: 'https://icon-library.com/images/vendetta-icon/vendetta-icon-14.jpg',
-    //       name: 'Anonymous',
-    //     }
-    //   }
-    // });
+    // Get the IDX profile
+    try {
+      const profile:any = await this.idx.get('basicProfile', author + '@eip155:' + this.auth.chainId)
+      // Currently IDX.get returns Promise<unknown>
+      // Follow changes on: https://developers.idx.xyz/reference/idx/#get
+      console.log('did',profile);
+      if(profile !== null) {
+        return {
+          address: author,
+          image: profile.image.original.src.replace('ipfs://', 'https://ipfs.io/ipfs/'),
+          name: profile.name,
+        };
+      }
+
+      return {
+        address: author,
+        image: 'https://icon-library.com/images/vendetta-icon/vendetta-icon-14.jpg',
+        name: 'Anonymous',
+      }
+    } catch (error) {
+      console.error('No DID Profile. Fallback to 3Box', error.message);
+    }
 
     // Retrieve profile info from (legacy) 3Box
     return await getLegacy3BoxProfileAsBasicProfile(author).then(profile => {
@@ -156,7 +166,29 @@ export class App {
   async loadConversation():Promise<void> {
     const newMessages = (await axios.get(`https://theconvo.space/api/comments?threadId=cl_descussion:${this.auth.chainId}&apikey=${ process.env.CONVO_API_KEY }`)).data;
 
-    const formatDistanceLocale = {
+    if(newMessages && JSON.stringify([...newMessages]) !== JSON.stringify([...this.messages])) {
+      newMessages.map(async message => {
+        if(message && !message.metadata.address) message.metadata = await this.loadProfile(message.author);
+      });
+      this.messages = JSON.parse(JSON.stringify(newMessages));
+
+      const messagesElem = await window.document.querySelector('.container');
+      window.scrollTo(0,messagesElem.scrollHeight);
+    }
+  }
+
+  jump(id: string): void {
+    const el = document.getElementById(id);
+    el.scrollIntoView({behavior: 'smooth'});
+    el.classList.add('animate')
+    setTimeout((): void => {
+      el.classList.remove('animate')
+    }, 2500);
+  }
+
+  getTimeDistance(date: string): string {
+  
+      const formatDistanceLocale = {
       lessThanXSeconds: '{{count}}s',
       xSeconds: '{{count}}s',
       halfAMinute: '30s',
@@ -191,29 +223,15 @@ export class App {
       return result
     }
 
-    if(newMessages.length && newMessages.length !== this.messages.length) {
-      newMessages.map(async message => {
-        if(message && !message.metadata.address) message.metadata = await this.loadProfile(message.author);
-        message.created = formatDistanceToNowStrict(new Date(parseInt(message.createdOn)), {addSuffix: false, locale: { formatDistance}});
-        if(message.replyTo) {
-          message.replyToOrigin = newMessages.filter(origin => {
-            return origin._id === message.replyTo;
-          })[0];
-        }
-      });
-      this.messages = newMessages;
-      const messagesElem = await window.document.querySelector('.messages')
-      messagesElem.scrollTop = messagesElem.scrollHeight;
-    }
+    return formatDistanceToNowStrict(new Date(parseInt(date)), {addSuffix: false, locale: { formatDistance}});
   }
 
-  jump(id: string): void {
-    const el = document.getElementById(id);
-    el.scrollIntoView({behavior: 'smooth'});
-    el.classList.add('animate')
-    setTimeout((): void => {
-      el.classList.remove('animate')
-    }, 2500);
+  getReplyOrigin(message: IMessage): IMessage {
+    if(message.replyTo) {
+      return this.messages.filter(origin => {
+        return origin._id === message.replyTo;
+      })[0];
+    }
   }
 
 }
